19f388df823f9eeffe274558bb428bd44926fd88
Lanouer REHAIEM <l_rehaiem@bercy.local>
2016-02-01 11:35:49 +0100

    [WIP]Added slave branch form

---
+++ src/AppBundle/Controller/ContactController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class ContactController
+{
+	/**
+     *@Template
+    */
+    public function contactAction()
+    {
+        return array();
+    }
+}
+
